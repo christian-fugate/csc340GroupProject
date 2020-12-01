a43139c6dcd48e725f9feba2419ad7c6f9f0c0e2
christian-fugate <57549406+christian-fugate@users.noreply.github.com>
2020-12-01 13:14:31 -0500

    Shows
    Made a back button work and seat transition

--- MovieTicketOrdering/shows.Designer.cs
+++ MovieTicketOrdering/shows.Designer.cs
@@ -29,9 +29,10 @@
         private void InitializeComponent()
         {
             this.showBox = new System.Windows.Forms.ListBox();
-            this.backToLogin = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.back = new System.Windows.Forms.Button();
             this.showDescription = new System.Windows.Forms.TextBox();
+            this.orderTickets = new System.Windows.Forms.Button();
+            this.selectAShow = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // showBox
@@ -43,23 +44,15 @@
             this.showBox.TabIndex = 0;
             this.showBox.SelectedIndexChanged += new System.EventHandler(this.showBox_SelectedIndexChanged);
             // 
-            // backToLogin
+            // back
             // 
-            this.backToLogin.Location = new System.Drawing.Point(12, 22);
-            this.backToLogin.Name = "backToLogin";
-            this.backToLogin.Size = new System.Drawing.Size(91, 23);
-            this.backToLogin.TabIndex = 1;
-            this.backToLogin.Text = "Back To Info";
-            this.backToLogin.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(455, 348);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(92, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Order Tickets";
-            this.button1.UseVisualStyleBackColor = true;
+            this.back.Location = new System.Drawing.Point(12, 22);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(91, 23);
+            this.back.TabIndex = 1;
+            this.back.Text = "Back";
+            this.back.UseVisualStyleBackColor = true;
+            this.back.Click += new System.EventHandler(this.back_Click);
             // 
             // showDescription
             // 
@@ -70,14 +63,33 @@
             this.showDescription.TabIndex = 3;
             this.showDescription.Text = "Show Description";
             // 
+            // orderTickets
+            // 
+            this.orderTickets.Location = new System.Drawing.Point(448, 371);
+            this.orderTickets.Name = "orderTickets";
+            this.orderTickets.Size = new System.Drawing.Size(92, 23);
+            this.orderTickets.TabIndex = 4;
+            this.orderTickets.Text = "Order Tickets";
+            this.orderTickets.UseVisualStyleBackColor = true;
+            this.orderTickets.Click += new System.EventHandler(this.orderTickets_Click);
+            // 
+            // selectAShow
+            // 
+            this.selectAShow.AutoSize = true;
+            this.selectAShow.Location = new System.Drawing.Point(425, 397);
+            this.selectAShow.Name = "selectAShow";
+            this.selectAShow.Size = new System.Drawing.Size(0, 13);
+            this.selectAShow.TabIndex = 5;
+            // 
             // shows
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(648, 450);
+            this.Controls.Add(this.selectAShow);
+            this.Controls.Add(this.orderTickets);
             this.Controls.Add(this.showDescription);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.backToLogin);
+            this.Controls.Add(this.back);
             this.Controls.Add(this.showBox);
             this.Name = "shows";
             this.Text = "Shows";
@@ -90,8 +102,9 @@
         #endregion
 
         private System.Windows.Forms.ListBox showBox;
-        private System.Windows.Forms.Button backToLogin;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button back;
         private System.Windows.Forms.TextBox showDescription;
+        private System.Windows.Forms.Button orderTickets;
+        private System.Windows.Forms.Label selectAShow;
     }
 }
--- MovieTicketOrdering/shows.cs
+++ MovieTicketOrdering/shows.cs
@@ -27,11 +27,28 @@ namespace MovieTicketOrdering
                 cur += "  The Show time is: " + show[2];
                 showBox.Items.Add(cur);
             }
+            Console.WriteLine(showBox.SelectedIndex);
         }
 
         private void showBox_SelectedIndexChanged(object sender, EventArgs e)
         {
             showDescription.Text = currentShows.shows[showBox.SelectedIndex][4];
+        }
+
+        private void back_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void orderTickets_Click(object sender, EventArgs e)
+        {
+            if (showBox.SelectedIndex != -1)
+            {
+                Form seats = new seats();
+                seats.Show();
+            }
+            else
+                selectAShow.Text = "Select a show and try again";
         }
     }
 }
